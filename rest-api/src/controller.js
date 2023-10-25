c4404d3e62ed19a9a8d096f0b59f96e8f3b308e7
lid21 <guadalupegabrielasoto@gmail.com>
2023-10-25 18:42:59 -0300

    rest api con try catch

--- rest-api/src/controller.js
+++ rest-api/src/controller.js
@@ -8,17 +8,22 @@ class librosController{
     }
     async add(req, res){
         const libros = req.body;
-        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, año-publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libros.nombre, libros.autor, libros.categoria, libros.año-publicacion, libros.ISBN]);
+        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, fechaPublicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libros.nombre, libros.autor, libros.categoria, libros.fechaPublicacion, libros.ISBN]);
         res.json({"Id insertado": result.insertId});
     }
     async delete(req, res) {
+        try{
         const libros = req.body;
-        const [result] = await pool.query(`DELETE FROM Libros WHERE id=(?)`, [libros.id]);
+        const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libros.ISBN]);
+        if(result.affectedRows <=0) return res.status(404).json({message:"Libro no encontrado"});
         res.json({"registros eliminados": result.affectedRows});
+        } catch (error) {
+            return res.status(500).json({ message:"no se puede eliminar el libro"});
+        }
     }
     async update(req, res){
         const libros = req.body;
-        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), año-publicacion=(?), ISBN=(?) WHERE id=(?)`, [libros.nombre, libros.autor, libros.categoria, libros.año-publicacion, libros.ISBN, libros.id]);
+        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), fechaPublicacion=(?), ISBN=(?) WHERE id=(?)`, [libros.nombre, libros.autor, libros.categoria, libros.fechaPublicacion, libros.ISBN, libros.id]);
         res.json({"registros actualizados": result.changedRows});
     }
 
